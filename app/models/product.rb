b86aef8703acb047ed13c457cbef7286e8231b77
e-kaneyuki <occultchikin@gmail.com>
2021-08-07 23:27:57 +0900

    #9 has_many :モデル名を複数形へ修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
-  has_many :order_detail
-  has_many :purchase
+  has_many :order_details
+  has_many :purchases
   belongs_to :category
   belongs_tp :sale_status
   belongs_to :product_status
--- app/models/sale_status.rb
+++ app/models/sale_status.rb
@@ -1,3 +1,3 @@
 class SaleStatus < ApplicationRecord
-  has_many :product
+  has_many :products
 end
