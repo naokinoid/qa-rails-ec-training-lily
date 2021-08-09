a2115a355b6cfa6577e559ef877f7c7b86b4e02f
naokinoid <naoki.itou.0322@gmail.com>
2021-08-09 21:48:12 +0900

    #11 VALID_NUMBER_REGEX を VALID_ZIPCODE_REGEX に変更 is:7 を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,15 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  VALID_NUMBER_REGEX = /\A[0-9]+\z/
+  VALID_ZIPCODE_REGEX = /\A\d{7}\z/
   VALID_PHONE_REGEX = /\A\d{10,11}\z/
 
   validates :password, presence: true, length: { in: 6..15 },
              format: { with: VALID_PASSWORD_REGEX }, allow_nil: true
   validates :last_name,  presence: true, length: { maximum: 10 }
   validates :first_name,  presence: true, length: { maximum: 10 }
-  validates :zipcode,  presence: true, length: { is: 7 },
-             format: { with: VALID_NUMBER_REGEX }
+  validates :zipcode,  presence: true,
+             format: { with: VALID_ZIPCODE_REGEX }
   validates :prefecture,  presence: true, length: { maximum: 5 }
   validates :municipality,  presence: true, length: { maximum: 10 }
   validates :address,  presence: true, length: { maximum: 15 }
