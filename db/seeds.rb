bb34f39ecc9286101a10093557e96bc0ca54dd9d
宮川大輝 <>
2021-08-11 20:19:47 +0900

    #14 order 関連のseed を作成しました。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.all.each do |user|
+user1.orders.create!(order_date: "2021-07-31", order_number: "1111")
+user2.orders.create!(order_date: "2021-07-30", order_number: "2222")
+end
+
+shipment_status1 = ShipmentStatus.create(shipment_status_name: "準備中")
+shipment_status2 = ShipmentStatus.create(shipment_status_name: "発送済")
+
+
+order1 = Order.first
+order1.order_details.create!(
+  product_id: 1,
+  shipment_statuses_name: "準備中",
+  order_detail_number: 1234,
+  order_quantity: 1,
+  shipment_date: "2021-08-02"
+)
+
+order1.order_details.create!(
+  product_id: 2,
+  shipment_statuses_name: "準備中",
+  order_detail_number: 2345,
+  order_quantity: 1,
+  shipment_date: "2021-08-02"
+)
+
+order1.order_details.create!(
+  product_id: 3,
+  shipment_statuses_name: "発送済",
+  order_detail_number: 3456,
+  order_quantity: 1,
+  shipment_date: "2021-08-02"
+)
+
+order1.order_details.create!(
+  product_id: 4,
+  shipment_statuses_name: "発送済",
+  order_detail_number: 4567,
+  order_quantity: 1,
+  shipment_date: "2021-08-02"
+)
+
+order2 = Order.second
+order2.order_details.create!(
+  product_id: 1,
+  shipment_statuses_name: "準備中",
+  order_detail_number: 5678,
+  order_quantity: 1,
+  shipment_date: "2021-08-02"
+)
+
+order2.order_details.create!(
+  product_id: 2,
+  shipment_statuses_name: "準備中",
+  order_detail_number: 6789,
+  order_quantity: 1,
+  shipment_date: "2021-08-02"
+)
+
+order2.order_details.create!(
+  product_id: 3,
+  shipment_statuses_name: "発送済",
+  order_detail_number: 7891,
+  order_quantity: 1,
+  shipment_date: "2021-08-02"
+)
+
+order2.order_details.create!(
+  product_id: 4,
+  shipment_statuses_name: "発送済",
+  order_detail_number: 8912,
+  order_quantity: 1,
+  shipment_date: "2021-08-02"
+)
