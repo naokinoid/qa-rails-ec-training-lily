638fbad1b5a3d436ef36ca82b6ca5954c6b95588
e-kaneyuki <occultchikin@gmail.com>
2021-08-12 20:37:21 +0900

    Userの各カラムへ値を指定して入力

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  last_name: "わんこ",
+  first_name: "犬の",
+  zipcode: "123-3456",
+  prefecture: "京都府",
+  municipality: "木津川市",
+  address: "加茂町大野919-1",
+  apartments: "",
+  email: "wanwanwanko@sample.com",
+  phone_number: "090-1234-5678",
+  company_name: "wankonyanko"
+  password: "aaa"
+  # password_digest: "",
+  # delete_flag: ,
+  # user_classification:
+)
