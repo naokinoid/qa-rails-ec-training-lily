b28689502d1607b1a8f0cf4cd4e2e4080495f479
naokinoid <naoki.itou.0322@gmail.com>
2021-08-20 22:16:13 +0900

    #31 全件の取得方法を変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   def index
-    @count = Product.count
     @products = Product.page(params[:page]).per(15).order('created_at ASC')
     @categories = Category.all
   end
