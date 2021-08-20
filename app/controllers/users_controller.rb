5993cf2de83875f148ac1f354cd2e13220b4fd39
e-kaneyuki <occultchikin@gmail.com>
2021-08-20 22:06:33 +0900

    editメソッドとストロングパラメーター作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:last_name, :first_name, :zipcode, :prefecture, :municipality, :address, :apartments, :email, :phone_number)
+    end
 end
