2bd7cbc9f44946b1554be422e2b73934c9c1499d
e-kaneyuki <occultchikin@gmail.com>
2021-08-22 21:28:36 +0900

    updateメソッドを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  def update
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to("/users/:id")
+      else
+        render 'edit'
+      end
+    end
+  end
 
   private
 
