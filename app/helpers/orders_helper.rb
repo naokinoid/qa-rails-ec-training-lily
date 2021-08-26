a51d17a878222b1de778d16e563c5e88f47dd56e
宮川大輝 <>
2021-08-26 11:53:22 +0900

    #24 注文詳細画面　shipment_status に準備中があるかどうかの判定を一つのメソッドにまとめました。

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,9 +1,8 @@
 module OrdersHelper
-  def shipment_status_array
-    array = @order.order_details.map {|order_detail| order_detail.shipment_status.shipment_status_name }
-  end
-
-  def products_total_fee
-    @order.order_details.sum {|order_detail| order_detail.product.price * order_detail.order_quantity }
+  def has_prepared_shipment_status?(order)
+    array = order.order_details.map {|order_detail| order_detail.shipment_status.shipment_status_name }
+    if array.include?("準備中")
+      link_to "注文をキャンセルする", "#", class: "btn btn-danger"
+    end
   end
 end
