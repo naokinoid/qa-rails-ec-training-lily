fe522620eeee36bf64da77939d4eae8bebf9a1f7
宮川大輝 <>
2021-08-26 13:00:24 +0900

    #24 注文詳細画面　sub_total_price メソッドをorder_detail.rb に定義しました。

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,7 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :shipment_status
   belongs_to :product
+  def sub_total_price(order)
+    product.price * order_quantity
+  end
 end
