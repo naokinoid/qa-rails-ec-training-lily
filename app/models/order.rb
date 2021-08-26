4edaf8cfac2ef9edfde77450c9987b23b1fb932a
宮川大輝 <>
2021-08-26 16:18:51 +0900

    #24 注文詳細画面　has_prepared_shipment_status? の引数を削除

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details, dependent: :destroy
-  def has_prepared_shipment_status?(order)
-    order.order_details.any? {|order_detail| order_detail.shipment_status.shipment_status_name == "準備中" }
+  def has_prepared_shipment_status?
+    order_details.any? {|order_detail| order_detail.shipment_status.shipment_status_name == "準備中" }
   end
   def products_total_fee
     order_details.sum {|order_detail| order_detail.product.price * order_detail.order_quantity }
