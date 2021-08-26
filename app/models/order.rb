1add2f8958afbd4acd83eff22a6249b42b368952
宮川大輝 <>
2021-08-26 12:59:19 +0900

    #24 注文詳細画面　products_total_feeメソッドをorder.rb で定義しました。

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details, dependent: :destroy
+  def products_total_fee(order)
+   order.order_details.sum {|order_detail| order_detail.product.price * order_detail.order_quantity }
+  end
 end
