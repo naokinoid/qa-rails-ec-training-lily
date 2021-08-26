a5611f67e82517479fc7f10760350fe0be4c9c78
宮川大輝 <>
2021-08-26 14:58:11 +0900

    #24 注文詳細画面　products_total_fee 関連の引数を削除しました。

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
   def has_prepared_shipment_status?(order)
     order.order_details.any? {|order_detail| order_detail.shipment_status.shipment_status_name == "準備中" }
   end
-  def products_total_fee(order)
-    order.order_details.sum {|order_detail| order_detail.product.price * order_detail.order_quantity }
+  def products_total_fee
+    order_details.sum {|order_detail| order_detail.product.price * order_detail.order_quantity }
   end
 end
