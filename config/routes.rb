46859707b038c19187b64ff1faa736455c0a040e
naokinoid <naoki.itou.0322@gmail.com>
2021-08-27 22:22:29 +0900

    #28 route.rbを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get '/login', to: 'sessions#new'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :products
   resources :users
   resources :orders
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
 end
