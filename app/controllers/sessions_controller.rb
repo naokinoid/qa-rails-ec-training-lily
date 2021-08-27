252976638766246277f67b7cf81339930f17263d
naokinoid <naoki.itou.0322@gmail.com>
2021-08-27 21:56:10 +0900

    #28 ログイン・ログアウト処理　実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが異なります。'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to login_path
+  end
+
 end
