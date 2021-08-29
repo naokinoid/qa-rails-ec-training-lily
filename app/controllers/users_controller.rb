39d09b1ee9094644b4c64d0c432995a82e0f39d4
e-kaneyuki <occultchikin@gmail.com>
2021-08-29 21:58:36 +0900

    updateメソッド作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,21 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  def update
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to("/users/:id")
+      else
+        render 'edit'
+        flash[:false] = "ミスです"
+      end
+    end
+  end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:last_name, :first_name, :zipcode, :prefecture, :municipality, :address, :apartments, :email, :phone_number)
+    end
 end
