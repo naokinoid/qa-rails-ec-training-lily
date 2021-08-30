21d702819f4b248b13265b3ad964fc4dcdb09e02
e-kaneyuki <occultchikin@gmail.com>
2021-08-30 22:43:26 +0900

    headerのユーザー名を表記するために@userではなくcurrent_userを使用

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @user = User.find(params[:id])
     @products = Product.order('created_at ASC').page(params[:page]).per(15)
     @categories = Category.all
   end
 
   def show
-    @user = User.find(params[:id])
     @product = Product.find_by(id: params[:id])
   end
 end
