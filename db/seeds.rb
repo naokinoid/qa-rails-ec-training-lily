91e1642947ff582923d5187d8d377e85ef940e47
naokinoid <naoki.itou.0322@gmail.com>
2021-08-31 23:28:09 +0900

    rubocop指摘 未利用の変数定義を削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
-user_classification1 = UserClassification.create!(
+UserClassification.create!(
   id: 1,
   user_classification_name: "一般ユーザー",
 )
-user_classification2 = UserClassification.create!(
+UserClassification.create!(
   id: 2,
   user_classification_name: "管理者",
 )
-user_classificatio3 = UserClassification.create!(
+UserClassification.create!(
   id: 3,
   user_classification_name: "購入者",
 )
-user_classification4 = UserClassification.create!(
+UserClassification.create!(
   id: 4,
   user_classification_name: "販売者",
 )
 
-user1 = User.create!(
+User.create!(
   last_name: Gimei.last.kanji,
   first_name: Gimei.first.kanji,
   zipcode: Faker::Number.number(digits: 7),
@@ -30,7 +30,7 @@ user1 = User.create!(
   user_classification_id: Faker::Number.between(from: 1, to: 4),
 )
 
-user2 = User.create!(
+User.create!(
   last_name: Gimei.last.kanji,
   first_name: Gimei.first.kanji,
   zipcode: Faker::Number.number(digits: 7),
@@ -112,7 +112,7 @@ shipment_status2 = ShipmentStatus.create!(shipment_status_name: "発送済")
 order1 = Order.create!(order_date: "2021-07-31", order_number: "1111", user_id: User.first.id)
 order2 = Order.create!(order_date: "2021-07-30", order_number: "2222", user_id: User.second.id)
 
-order_detail1 = OrderDetail.create!(
+OrderDetail.create!(
   order_detail_number: "1111",
   order_quantity: 1,
   shipment_date: "2021-08-02",
@@ -121,7 +121,7 @@ order_detail1 = OrderDetail.create!(
   shipment_status_id: shipment_status1.id,
 )
 
-order_detail2 = OrderDetail.create!(
+OrderDetail.create!(
   order_detail_number: "2222",
   order_quantity: 1,
   shipment_date: "2021-08-02",
@@ -130,7 +130,7 @@ order_detail2 = OrderDetail.create!(
   shipment_status_id: shipment_status2.id,
 )
 
-order_detail3 = OrderDetail.create!(
+OrderDetail.create!(
   order_detail_number: "3333",
   order_quantity: 1,
   shipment_date: "2021-08-02",
@@ -139,7 +139,7 @@ order_detail3 = OrderDetail.create!(
   shipment_status_id: shipment_status1.id,
 )
 
-order_detail4 = OrderDetail.create!(
+OrderDetail.create!(
   order_detail_number: "4444",
   order_quantity: 1,
   shipment_date: "2021-08-02",
@@ -148,7 +148,7 @@ order_detail4 = OrderDetail.create!(
   shipment_status_id: shipment_status2.id,
 )
 
-order_detail5 = OrderDetail.create!(
+OrderDetail.create!(
   order_detail_number: "5555",
   order_quantity: 1,
   shipment_date: "2021-08-02",
@@ -157,7 +157,7 @@ order_detail5 = OrderDetail.create!(
   shipment_status_id: shipment_status2.id,
 )
 
-order_detail6 = OrderDetail.create!(
+OrderDetail.create!(
   order_detail_number: "6666",
   order_quantity: 1,
   shipment_date: "2021-08-02",
@@ -166,7 +166,7 @@ order_detail6 = OrderDetail.create!(
   shipment_status_id: shipment_status2.id,
 )
 
-order_detail7 = OrderDetail.create!(
+OrderDetail.create!(
   order_detail_number: "7777",
   order_quantity: 1,
   shipment_date: "2021-08-02",
@@ -175,7 +175,7 @@ order_detail7 = OrderDetail.create!(
   shipment_status_id: shipment_status2.id,
 )
 
-order_detail8 = OrderDetail.create!(
+OrderDetail.create!(
   order_detail_number: "8888",
   order_quantity: 1,
   shipment_date: "2021-08-02",
