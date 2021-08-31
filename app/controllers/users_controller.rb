0cbbadabce961fa7dc494cfe2f03178cee0f5a32
naokinoid <naoki.itou.0322@gmail.com>
2021-08-31 11:17:45 +0900

    #29 users_controller onlyの書き方を%記法に修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show, :edit]
-  before_action :correct_user, only: [:show, :edit]
+  before_action :logged_in_user, only: %i(show edit)
+  before_action :correct_user, only: %i(show edit)
 
   def show
     @user = User.find(params[:id])
