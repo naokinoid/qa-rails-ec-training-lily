d81040085f57bbbe730b86d662d7f37dd9988127
naokinoid <naoki.itou.0322@gmail.com>
2021-08-31 23:01:49 +0900

    rubocop指摘 自動修正（ -A）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   has_secure_password
 
-  VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  VALID_ZIPCODE_REGEX = /\A\d{7}\z/
-  VALID_PHONE_REGEX = /\A\d{10,11}\z/
+  VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/.freeze
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  VALID_ZIPCODE_REGEX = /\A\d{7}\z/.freeze
+  VALID_PHONE_REGEX = /\A\d{10,11}\z/.freeze
 
   validates :password, presence: true, length: { in: 6..15 },
                        format: { with: VALID_PASSWORD_REGEX }, allow_nil: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-user_classification1 = UserClassification.create(
+user_classification1 = UserClassification.create!(
   id: 1,
   user_classification_name: "一般ユーザー",
 )
-user_classification2 = UserClassification.create(
+user_classification2 = UserClassification.create!(
   id: 2,
   user_classification_name: "管理者",
 )
-user_classificatio3 = UserClassification.create(
+user_classificatio3 = UserClassification.create!(
   id: 3,
   user_classification_name: "購入者",
 )
-user_classification4 = UserClassification.create(
+user_classification4 = UserClassification.create!(
   id: 4,
   user_classification_name: "販売者",
 )
@@ -45,25 +45,25 @@ user2 = User.create!(
   user_classification_id: Faker::Number.between(from: 1, to: 4),
 )
 
-Category.create(category_name: "キャンプ")
-Category.create(category_name: "運動・トレーニング")
+Category.create!(category_name: "キャンプ")
+Category.create!(category_name: "運動・トレーニング")
 
-SaleStatus.create(sale_status_name: "在庫あり")
-SaleStatus.create(sale_status_name: "通常1週間以内に発送")
-SaleStatus.create(sale_status_name: "発売前/予約注文可")
-SaleStatus.create(sale_status_name: "一時的に在庫切れ")
-SaleStatus.create(sale_status_name: "現在お取り扱いできません")
+SaleStatus.create!(sale_status_name: "在庫あり")
+SaleStatus.create!(sale_status_name: "通常1週間以内に発送")
+SaleStatus.create!(sale_status_name: "発売前/予約注文可")
+SaleStatus.create!(sale_status_name: "一時的に在庫切れ")
+SaleStatus.create!(sale_status_name: "現在お取り扱いできません")
 
-ProductStatus.create(product_status_name: "新品")
-ProductStatus.create(product_status_name: "中古 – 非常に良い")
-ProductStatus.create(product_status_name: "中古 – 良い")
-ProductStatus.create(product_status_name: "中古 – 可")
+ProductStatus.create!(product_status_name: "新品")
+ProductStatus.create!(product_status_name: "中古 – 非常に良い")
+ProductStatus.create!(product_status_name: "中古 – 良い")
+ProductStatus.create!(product_status_name: "中古 – 可")
 
-product1 = Product.create(
+product1 = Product.create!(
   product_name: "LEDランタン",
   price: 5000,
   description: "キャンプサイトを優しく彩るアンティーク風LEDランタン。明るさ : 20〜370ルーメン 無段階調節",
-  regist_date: Time.now,
+  regist_date: Time.zone.now,
   delete_flag: false,
   category_id: Category.first.id,
   sale_status_id: SaleStatus.first.id,
@@ -71,11 +71,11 @@ product1 = Product.create(
   user_id: User.first.id,
 )
 
-product2 = Product.create(
+product2 = Product.create!(
   product_name: "ヘキサタープ",
   price: 30000,
   description: "サイズ:使用時/約460×435×高さ230cm、収納時/約直径18×74cm、メインポール・サイドポール/約直径19×長さ180mm、重量:約7.6kg",
-  regist_date: Time.now,
+  regist_date: Time.zone.now,
   delete_flag: false,
   category_id: Category.first.id,
   sale_status_id: SaleStatus.first.id,
@@ -83,11 +83,11 @@ product2 = Product.create(
   user_id: User.first.id,
 )
 
-product3 = Product.create(
+product3 = Product.create!(
   product_name: "可変式ダンベル",
   price: 12800,
   description: "【5段階調整の筋トレダンベル】：5kg～25kgの間でお好みの重さに5段階で調節可能！初心者から上級者まで自分に合ったウエイトでトレーニングできます",
-  regist_date: Time.now,
+  regist_date: Time.zone.now,
   delete_flag: false,
   category_id: Category.second.id,
   sale_status_id: SaleStatus.first.id,
@@ -95,19 +95,19 @@ product3 = Product.create(
   user_id: User.second.id,
 )
 
-product4 = Product.create(
+product4 = Product.create!(
   product_name: "プロテイン",
   price: 3980,
   description: "【2021年モンドセレクション最高金賞】世界が認めた絶品チョコ風味！3～5シェイクで素早く溶けてダマにならない！",
-  regist_date: Time.now,
+  regist_date: Time.zone.now,
   delete_flag: false,
   category_id: Category.second.id,
   sale_status_id: SaleStatus.first.id,
   product_status_id: ProductStatus.first.id,
   user_id: User.second.id,
 )
-shipment_status1 = ShipmentStatus.create(shipment_status_name: "準備中")
-shipment_status2 = ShipmentStatus.create(shipment_status_name: "発送済")
+shipment_status1 = ShipmentStatus.create!(shipment_status_name: "準備中")
+shipment_status2 = ShipmentStatus.create!(shipment_status_name: "発送済")
 
 order1 = Order.create!(order_date: "2021-07-31", order_number: "1111", user_id: User.first.id)
 order2 = Order.create!(order_date: "2021-07-30", order_number: "2222", user_id: User.second.id)
