885e3ba516eb33c4a45fa628815a77927c900f7a
e-kaneyuki <occultchikin@gmail.com>
2021-09-11 23:02:45 +0900

    商品検索ロジック作成(category_idで検索)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,14 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params[:id])
   end
+
+  def search
+    # byebug
+    @products = Product.search(params[:keyword], params[:category_id]).page(params[:page]).per(15)
+    @category_name = params[:category_name]
+    @keyword = params[:keyword]
+    # byebug
+    render "index"
+
+  end
 end
