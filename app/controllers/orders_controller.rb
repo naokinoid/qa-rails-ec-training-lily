0a3cf6b32a60488c9cc047cd137147371de9b57c
naokinoid <naoki.itou.0322@gmail.com>
2021-09-14 13:41:51 +0900

    #39 createメソッドをOrdersControllerへ移行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,4 +13,29 @@ class OrdersController < ApplicationController
     @order = Order.find_by(id: params[:id]).destroy!
     redirect_to orders_path
   end
+
+  # rubocop:disable Metrics::AbcSize
+  def create
+    @order = Order.new(
+      order_date: Time.zone.now,
+      order_number: Order.last.order_number.to_i + 1,
+      user_id: current_user.id,
+    )
+    @order.save!
+
+    session[:cart].each.with_index(1) do |item, i|
+      @order_detail = OrderDetail.new(
+        order_detail_number: @order.order_number + "%05d" % i,
+        order_quantity: item["quantity"],
+        order_id: @order.id,
+        shipment_status_id: 1,
+        product_id: item["product_id"],
+      )
+      @order_detail.save!
+    end
+
+    session[:cart] = nil
+    redirect_to order_path(@order)
+  end
+  # rubocop:enable Metrics::AbcSize
 end
