479c2b3a2d1f4809eca1c84a974d6f6d41f10cad
naokinoid <naoki.itou.0322@gmail.com>
2021-09-27 08:56:22 +0900

    動的タイトル

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "探求学園ECサイト"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
