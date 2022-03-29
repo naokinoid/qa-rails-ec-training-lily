aa6d4c4178069e5e1d5f5e0f02be1f60ca95b6b8
naokinoid <naoki.itou.0322@gmail.com>
2022-03-29 23:11:40 +0900

    初期設定　追加 関連ファイル作成の設定

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,14 @@ module Myapp
 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    # RSpec関連のファイル作成の設定　falseになっているものは作成されない
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
